d125a79324e70f0141ee01e609d31eaf3b1fc68a
Artem Levin <leoartemlev@yandex.ru>
2023-06-04 12:43:55 +0500

    12 task

---
+++ DashaTasks.UnitTests/Task12/CarTests.cs
@@ -0,0 +1,46 @@
+﻿using DashaTasks.Task12;
+using NUnit.Framework;
+
+namespace DashaTasks.UnitTests.Task12;
+
+public class CarTests
+{
+    [Test]
+    public void Car_ArgumentsCorrect_CreateCar()
+    {
+        var car = new Car("Hundai", 2000, "12345678901234567", BodyType.Sedan);
+        
+        Assert.That(car.Brand, Is.EqualTo("Hundai"));
+        Assert.That(car.YearOfRelease, Is.EqualTo(2000));
+        Assert.That(car.VIN, Is.EqualTo("12345678901234567"));
+        Assert.That(car.BodyType, Is.EqualTo(BodyType.Sedan));
+    }
+
+    [TestCase(0)]
+    [TestCase(-1)]
+    public void Car_YearOfReleaseLessOrEqualToZero_ThrowsArgumentException(int yearOfRelease)
+    {
+        TestDelegate action = () => { new Car("Hundai", yearOfRelease, "12345678901234567", BodyType.Sedan); };
+        
+        Assert.That(action, Throws.ArgumentException);
+    }
+
+    public void Car_VinLessThan17_ThrowsArgumentException()
+    {
+        TestDelegate action = () => { new Car("Hundai", 2000, "1111", BodyType.Sedan); };
+        
+        Assert.That(action, Throws.ArgumentException);
+    }
+
+    [Test]
+    public void GetInfo_Car_ReturnsCorrectString()
+    {
+        const string expectedString = "Марка: Hundai. Год выпуска: 2000. VIN: 12345678901234567. Тиа кузова: Седан\n"
+                                      + "Цена: 0. Дата продажи: 01.01.0001. ФИО покупателя: ";
+        var car = new Car("Hundai", 2000, "12345678901234567", BodyType.Sedan);
+        
+        var result = car.GetInfo();
+        
+        Assert.That(result, Is.EqualTo(expectedString));
+    }
+}
---
+++ DashaTasks/Task12/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace DashaTasks.Task12;
+
+public enum BodyType
+{
+    Sedan,
+    Hatchback,
+    StationWagon,
+    Carbiolet
+}
+
+public class Car
+{
+    public readonly string VIN;
+
+    public string Brand { get; set; }
+
+    public int YearOfRelease { get; set; }
+
+    public BodyType BodyType { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateTime DateOfSale { get; set; }
+
+    public string CustomerFullName { get; set; }
+    
+    public Car(string brand, int yearOfRelease, string vin, BodyType bodyType)
+    {
+        Brand = brand;
+        if (yearOfRelease <= 0)
+            throw new ArgumentException();
+        YearOfRelease = yearOfRelease;
+
+        if (vin.Length < 17)
+            throw new ArgumentException();
+        VIN = vin;
+        BodyType = bodyType;
+    }
+
+    public string GetInfo()
+    {
+        return $"Марка: {Brand}. Год выпуска: {YearOfRelease}. VIN: {VIN}. Тиа кузова: {GetBodyTypeName()}\n" +
+               $"Цена: {Price}. Дата продажи: {DateOfSale.ToShortDateString()}. ФИО покупателя: {CustomerFullName}";
+    }
+
+    private string GetBodyTypeName()
+    {
+        return BodyType switch
+        {
+            BodyType.Sedan => "Седан",
+            BodyType.Hatchback => "Хэтчбек",
+            BodyType.Carbiolet => "Кабриолет",
+            BodyType.StationWagon => "Универсал",
+            _ => string.Empty
+        };
+    }
+}
