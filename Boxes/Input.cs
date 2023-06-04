1648a34cae7df7403c3a11a929bba8f7d6aada27
Artem Levin <leoartemlev@yandex.ru>
2023-06-04 17:56:14 +0500

    15 task

---
+++ Boxes/Input.cs
@@ -0,0 +1,42 @@
+﻿namespace Boxes;
+
+public static class Input
+{
+    public static IReadOnlyList<Stack<int>> ReadInputData()
+    {
+        const string fileName = "boxes.txt";
+
+        var result = new List<Stack<int>>();
+        using var reader = new StreamReader(fileName);
+        while (reader.EndOfStream is false)
+        {
+            var numbers = reader.ReadLine()?.Split(' ') ?? throw new InvalidDataException();
+
+            var valueList = numbers.Select(int.Parse).ToList();
+                
+            result.Add(GetStackFromList(valueList));
+        }
+
+        return result;
+    }
+
+    private static Stack<int> GetStackFromList(List<int> valueList)
+    {
+        var stack = new Stack<int>();
+
+        foreach (var value in valueList)
+        {
+            stack.Push(value);
+        }
+
+        return stack;
+    }
+
+    public static void WriteResult(string result)
+    {
+        const string fileName = "moves.txt";
+
+        using var writer = new StreamWriter(fileName);
+        writer.WriteLine(result);
+    }
+}
---
+++ Boxes/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using Boxes;
+
+var stacks = Input.ReadInputData();
+
+var movesCount = 0;
+var resultBuilder = new StringBuilder();
+LogState(stacks);
+MoveAllItemsToFirstStack(stacks);
+DistributeItemsFromFirstStack(stacks);
+DistributeItemsBetweenFirstThreeStacks(stacks);
+
+Console.WriteLine($"Число перекладываний {movesCount}");
+
+LogState(stacks);
+
+Input.WriteResult(resultBuilder.ToString());
+
+void LogState(IReadOnlyList<Stack<int>> stackList)
+{
+    for (var i = 0; i < stackList.Count; i++)
+    {
+        var sb = new StringBuilder();
+        sb.Append($"Стопка {i + 1}: ");
+        foreach (var item in stackList[i]) 
+            sb.Append($"{item} ");
+
+        Console.WriteLine(sb.ToString());
+    }
+}
+
+void MoveAllItemsToFirstStack(IReadOnlyList<Stack<int>> list)
+{
+    for (var i = 1; i < list.Count; i++)
+    {
+        while (list[i].Count > 0)
+        {
+            MoveFromOneStackToAnother(i, 0, list);
+            movesCount++;
+        }
+    }
+}
+
+void DistributeItemsFromFirstStack(IReadOnlyList<Stack<int>> list)
+{
+    var firstStack = list[0];
+    while (firstStack.Count != 0)
+    {
+        var value = list[0].Peek();
+        if (value == 1)
+            MoveFromOneStackToAnother(0, 1, list);
+        else
+            MoveFromOneStackToAnother(0, value - 1, list);
+        
+        movesCount++;
+    }
+}
+
+void DistributeItemsBetweenFirstThreeStacks(IReadOnlyList<Stack<int>> list)
+{
+    while (list[1].Count != 0)
+    {
+        var value = list[1].Peek();
+        MoveFromOneStackToAnother(1, value == 2 ? 2 : 0, list);
+        movesCount++;
+    }
+
+    while (list[2].Peek() == 2)
+    {
+        MoveFromOneStackToAnother(2, 1, list);
+        movesCount++;
+    }
+}
+
+
+
+void MoveFromOneStackToAnother(int fromIndex, int toIndex, IReadOnlyList<Stack<int>> stackList)
+{
+    var value = stackList[fromIndex].Pop();
+    stackList[toIndex].Push(value);
+    resultBuilder.AppendLine($"Переложить ящик {value} из стопки {fromIndex + 1} в стопку {toIndex + 1}");
+}
